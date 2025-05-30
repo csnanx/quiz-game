8d1af3d980dc19a98180fda67e48bc83a317bc9d
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-30 20:30:57 +0530

    Add input validation to only accept true/false

--- quiz_brain.py
+++ quiz_brain.py
@@ -7,10 +7,17 @@ class QuizBrain:
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
 
+    def get_valid_answer(self, current_question):
+        while True:
+            answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+            if answer.lower() in ["true", "false"]:
+                return answer
+            print("Invalid input. Please enter 'True' or 'False'")
+
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        user_answer = self.get_valid_answer(current_question)
         self.check_answer(user_answer, current_question.answer)
     
     def check_answer(self, user_answer, right_answer):
